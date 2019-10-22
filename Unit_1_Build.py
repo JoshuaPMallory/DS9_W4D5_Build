dc53b4266749995def6c569b784f7cdfd6e33e5a
Joshua <joshuapmallory@gmail.com>
2019-10-22 14:30:58 -0700

    Fixed the dopnut plot Added a graph Added a seaborn scatterplot

--- Unit_1_Build.py
+++ Unit_1_Build.py
@@ -59,10 +59,21 @@ df['Population']                = df['Population'].fillna(method = 'ffill', limi
 df['Crude Rate']                = df['Crude Rate'].str.strip(' (Unreliable)').astype('float')
 df['Crude Rate Standard Error'] = df['Crude Rate Standard Error'].astype('float')
 
+df['Age Group'] = df['Age Group'].replace({'1-4 years':   '1 - 4 years'
+                                          ,'5-9 years':   '5 - 9 years'
+                                          ,'10-14 years': '10 - 14 years'
+                                          ,'15-19 years': '15 - 19 years'
+                                          ,'20-24 years': '20 - 24 years'
+                                          ,'25-34 years': '25 - 34 years'
+                                          ,'35-44 years': '35 - 44 years'
+                                          ,'45-54 years': '45 - 54 years'
+                                          ,'55-64 years': '55 - 64 years'
+                                          ,'65-74 years': '65 - 74 years'
+                                          ,'75-84 years': '75 - 84 years'
+                                          })
+
 # df.to_csv(r'dataframes\CDC Mortality Dataframe California 1999 - 2016 CLEANED.csv')
 
-print(df.head())
-print(df.dtypes)
 # ------------------------------ Data Cleaning ------------------------------ #
 
 
@@ -72,40 +83,91 @@ print(df.dtypes)
 
 # The default values for ALL RESULTS
 years      = range(1999, 2017, 1)
-states     = range(1, 51, 1)
+states     = range(   1,   51, 1)
 counties   = range(6001, 6116, 1)
 age_groups = ['< 1 year'
-             ,'1-4 years'
-             ,'5-9 years'
-             ,'10-14 years'
-             ,'15-19 years'
-             ,'20-24 years'
-             ,'25-34 years'
-             ,'35-44 years'
-             ,'45-54 years'
-             ,'55-64 years'
-             ,'65-74 years'
-             ,'75-84 years'
+             ,'1 - 4 years'
+             ,'5 - 9 years'
+             ,'10 - 14 years'
+             ,'15 - 19 years'
+             ,'20 - 24 years'
+             ,'25 - 34 years'
+             ,'35 - 44 years'
+             ,'45 - 54 years'
+             ,'55 - 64 years'
+             ,'65 - 74 years'
+             ,'75 - 84 years'
              ,'85+ years'
              ,'Not Stated'
              ]
-cause      = df['Cause of death Code'].unique()
+causes     = df['Cause of death Code'].unique().tolist()
 
-# Default
-filter = [['Year',           years]
-         ,['State Code',     states]
-         ,['County Code',    counties]
-         ,['Age Group',      age_groups]
-         ,['Cause of death', cause]
-         ]
+cause_code_dict = pd.DataFrame(df['Cause of death'].unique().tolist()
+                              ,df['Cause of death Code'].unique().tolist()).to_dict('dict')
 
-def df_filter(year, state, county, age_group, cause):
+
+# This prevents errors in the other functions for not passing a list
+# Just makes it simpler for me
+def error_prev(some_list):
+    default = [years
+              ,states
+              ,counties
+              ,age_groups
+              ,causes]
+
+    # Converts the input into lists for use, if it's not already a list
+    iterator = -1
+    for choice in some_list:
+        iterator += 1
+        if type(choice) != type([]):
+            some_list[iterator] = [some_list[iterator]]
+
+    # If there's blank values, replaces them with the default
+    iterator = -1
+    for choice in some_list:
+        iterator += 1
+        if choice[0] == '':
+            some_list[iterator] = default[iterator]
+
+    return some_list
+
+
+
+
+# This function is specifically for making a graph that projects
+def df_graphing(choices):
+    choices            = error_prev(choices)
+    death_sums_by_year = []
+
+    for year in choices[0]:
+        choice = [year
+                 ,choices[1]
+                 ,choices[2]
+                 ,choices[3]
+                 ,choices[4]
+                 ]
+        death_sums_by_year.append(df_filter(choice)['Deaths'].sum())
+
+    plotting_df = np.array([df['Year'].unique().tolist()
+                           ,death_sums_by_year]).T
+    plotting_df = pd.DataFrame(plotting_df
+                              ,columns = ['Year'
+                                         ,'Deaths'])
+
+    return plotting_df
+
+
+
+
+def df_filter(choices):
+    choices = error_prev(choices)
+
     # Any choices must be put in lists, else .isin() can't function
-    filter = [['Year',                year]
-             ,['State Code',          state]
-             ,['County Code',         county]
-             ,['Age Group',           age_group]
-             ,['Cause of death Code', cause]
+    filter = [['Year',                choices[0]]
+             ,['State Code',          choices[1]]
+             ,['County Code',         choices[2]]
+             ,['Age Group',           choices[3]]
+             ,['Cause of death Code', choices[4]]
              ]
 
     # From right to left
@@ -116,38 +178,49 @@ def df_filter(year, state, county, age_group, cause):
 
     # Can't think of a less chunky version of this. It needs to save over prior
     # data to make the dataframe correctly.
-    filtering   = df
-    filtering   = filtering[filtering[filter[0][0]].isin(filter[0][1])]
-    filtering   = filtering[filtering[filter[1][0]].isin(filter[1][1])]
-    filtering   = filtering[filtering[filter[2][0]].isin(filter[2][1])]
-    filtering   = filtering[filtering[filter[3][0]].isin(filter[3][1])]
-    filtered_df = filtering[filtering[filter[4][0]].isin(filter[4][1])]
+    filtered_df = df
+    filtered_df = filtered_df[filtered_df[filter[0][0]].isin(filter[0][1])]
+    filtered_df = filtered_df[filtered_df[filter[1][0]].isin(filter[1][1])]
+    filtered_df = filtered_df[filtered_df[filter[2][0]].isin(filter[2][1])]
+    filtered_df = filtered_df[filtered_df[filter[3][0]].isin(filter[3][1])]
+    filtered_df = filtered_df[filtered_df[filter[4][0]].isin(filter[4][1])]
 
     return filtered_df
 
 
-# I need a way of summing the number of deaths per type of death,
-# or within any other category
 
-#  [ ]
-#  [ ]
 
 # ----------------------------------- Work ----------------------------------- #
 
 
 
+
 # ---------------------------------- Output ---------------------------------- #
 
-mpl.rcParams['font.size'] = 9.0
-
 # ---------- Graph ---------- #
-# plt.scatter(filtered_df['Age Group'], filtered_df['Deaths'])
-# plt.xticks(rotation = 45)
+# For each year, add up the deaths by age group
+
+choice      = [''
+              ,6
+              ,6073
+              ,'45 - 54 years'
+              ,causes
+              ]
+
+plotting_df = df_graphing(choice)
 
 
-# plt.show()
+plt.plot(plotting_df['Year'], plotting_df['Deaths'])
+plt.xticks(rotation = 45)
+plt.title('All deaths for ages ' +
+          choice[3][0] +
+          ' from ' +
+          str(range(1999, 2017, 1)[0]) +
+          ' to ' +
+          str(range(1999, 2017, 1)[-1]))
+
+plt.show()
 # ---------- Graph ---------- #
-
 
 # ---------- Donut Plot ---------- #
 
@@ -157,26 +230,24 @@ mpl.rcParams['font.size'] = 9.0
 
 # All of this will be pre-sorted by year, state, etc.
 
-# I'd like to use the dataframe filter within this.
 
-choice = [[1999]
-         ,[6]
-         ,[6073]
-         ,['< 1 year']
-         ,cause
-         ]
+choice      =          [1999
+                       ,6
+                       ,6073
+                       ,'75 - 84 years'
+                       ,causes
+                       ]
 
-filtered_df = df_filter(choice[0]
-                       ,choice[1]
-                       ,choice[2]
-                       ,choice[3]
-                       ,choice[4]
-                       )
+filtered_df = df_filter(choice)
 
-
+# Total dead of a given age group vs total population of a given age group
+# This gives us the total left living, for a given age group
 total_pop = filtered_df['Population'].iloc[0]
 alive     = filtered_df['Population'].iloc[0] - filtered_df['Deaths'].sum()
 dead      = filtered_df['Deaths'].sum()
+
+
+
 
 
 # This is the outer ring of the donut chart showing the living vs the dead for
@@ -185,14 +256,22 @@ group_names    = ['Alive', 'Dead']
 group_size     = [alive, dead]
 
 
-# This is the inner ring showing the most common causes
-# Need to make it so that only the top 4 are listed with a misc. 5th slot
-subgroup_names = filtered_df['Cause of death'].unique().tolist()
-subgroup_size  = filtered_df['Deaths'].unique().tolist()
+# This is the inner ring showing the 4 most common causes with a misc 5th cause
+# that includes all the others that were missed
+subgroup_names = filtered_df.sort_values(by = ['Deaths'], ascending = False)[:4]
+subgroup_names = subgroup_names['Cause of death Code'].tolist()
+
+subgroup_names.append(str(len(filtered_df['Deaths'].sort_values(ascending = False)[4:])) + ' others\ncombined')
+
+# This makes the list of the 5 groups going into the donut chart
+subgroup_size = filtered_df.sort_values(by = ['Deaths'], ascending = False)[:4]
+subgroup_size = subgroup_size['Deaths'].tolist()
+
+subgroup_size.append(filtered_df['Deaths'].sort_values(ascending = False)[4:].sum())
 
 
 # Create colors
-a, b = [plt.cm.Greens, plt.cm.Reds]
+a, d = [plt.cm.Greens, plt.cm.Reds]
 
 
 # First Ring (outside)
@@ -200,57 +279,131 @@ fig, ax = plt.subplots()
 
 ax.axis('equal')
 
+mpl.rcParams['font.size'] = 9.0
 
-pie, text = ax.pie(group_size
-                  ,radius = 1.3
-                  ,labels = group_names
-                  ,colors = [a(0.6)
-                            ,b(0.6)
-                            ]
-               )
-
+pie, text, perc = ax.pie(group_size
+                        ,radius        = 1.5
+                        ,startangle    = 90 + 50
+                        ,labels        = group_names
+                        ,labeldistance = 1.1
+                        ,autopct       = '%1.2f%%'
+                        ,pctdistance   = 0.87
+                        ,colors        = [a(0.6)
+                                         ,d(0.6)
+                                         ]
+                        )
 text[0].set_fontsize(15)
 text[1].set_fontsize(15)
 
+perc[0].set_fontsize(12)
+perc[1].set_fontsize(12)
 
 plt.setp(pie
-        ,width = 0.3
+        ,width = 0.4
         ,edgecolor = 'white')
 
 
 # Second Ring (Inside)
-pie2, text = ax.pie(subgroup_size
-                   ,radius        = 1.3 - 0.3
-                   ,labels        = subgroup_names
-                   ,labeldistance = 1
-                   ,colors        = [b(0.5)
-                                    ,b(0.4)
-                                    ,b(0.1)
-                                    ,b(0.9)
-                                    ]
-                )
-text[0].set_fontsize(12)
-text[1].set_fontsize(12)
-text[2].set_fontsize(12)
-text[3].set_fontsize(12)
-
+pie2, text2, perc2 = ax.pie(subgroup_size
+                           ,radius        = 1.5 - 0.4
+                           ,labels        = subgroup_names
+                           ,rotatelabels  = True
+                           ,counterclock  = False
+                           ,labeldistance = 0.8
+                           ,autopct       = '%1d%%'
+                           ,pctdistance   = 0.45
+                           ,colors        = [d(0.3)
+                                            ,d(0.4)
+                                            ,d(0.5)
+                                            ,d(0.6)
+                                            ,d(0.7)
+                                            ]
+                           )
 plt.setp(pie2
         ,width = 0.4
         ,edgecolor = 'white'
         )
 
+plt.setp(text2
+        ,rotation_mode = "anchor"
+        ,ha            = "center"
+        ,va            = "center")
+
+for tx in text2:
+    rot = tx.get_rotation()
+    tx.set_rotation(rot+90+(1-rot//180)*180)
+
+for number in range(0, len(subgroup_size), 1):
+    text2[number].set_fontsize(13)
+    perc2[number].set_fontsize(10)
+
+
 plt.margins(0, 0)
 plt.title('San Diego ' + str(choice[0][0]) + '\n' + 'Ages ' + choice[3][0]
          ,fontsize = 18)
+plt.legend(pie2, subgroup_names, loc = 'best')
+
 
 plt.show()
 
 # ---------- Donut Plot ---------- #
 
 
-# ---------- California County Map Plot ---------- #
+# ---------- Seaborn Plot ---------- #
+import seaborn as sns
+
+choice      =          [''
+                       ,6
+                       ,''
+                       ,''
+                       ,''
+                       ]
+
+filtered_df = df_filter(choice)
+
+target   = filtered_df['Deaths']
+features = filtered_df.columns.drop(['Deaths'
+                                    ,'Year'
+                                    ,'Cause of death'
+                                    ,'State'
+                                    ,'State Code'
+                                    ,'County Code'
+                                    ,'Age Group'
+                                    ,'Age Group Code'
+                                    ,'Population'
+                                    ,'Cause of death Code'
+                                    ,'Crude Rate'
+                                    ,'Crude Rate Standard Error'])
+
+plt.figure(figsize = (20, 10))
+for feature in features:
+    plt.scatter(x = feature
+               ,y = target
+               ,data = filtered_df
+               ,alpha = 0.1)
+plt.xticks(rotation = 90)
+plt.xlim(-1, 53)
+plt.ylim(0, 3500)
+
+plt.show()
+
+# ---------- Seaborn Plot ---------- #
+
 
 # ---------- California County Map Plot ---------- #
+# import geopandas as geo
+#
+# world = geo.read_file(geo.datasets.get_path('naturalearth_lowres'))
+# cities = geo.read_file(geo.datasets.get_path('naturalearth_cities'))
+# world.head()
+# world.plot();
+#
+# world = world[(world.pop_est>0) & (world.name!="Antarctica")]
+#
+# world['gdp_per_cap'] = world.gdp_md_est / world.pop_est
+#
+# world.plot(column='gdp_per_cap');
 
+# ---------- California County Map Plot ---------- #
 
 # ---------------------------------- Output ---------------------------------- #
